343cd05f65dd145f5792c23ae3d967a334b58f25
ParsaRoidd <ParsaRoidd.github.io@proton.me>
2026-06-19 19:02:02 +0330

    Entry point for two recently updateds Fucntion

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod buffer;
+mod draw;
 pub use buffer::*;
+pub use draw::*;
 use std::io::stdout;
 
 fn main()
 {
     let mut stream = stdout();
-    let input: String = buffer(&mut stream);
-    println!("{:?}", input);
+    let s = draw_input_box(&mut stream);
+    let input: String = buffer(&mut stream, s); 
+    println!("{input}");
 }
