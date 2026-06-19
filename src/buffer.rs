36ef42625f20bfa5cc03be8a09b2047c6a81bb9b
ParsaRoidd <ParsaRoidd.github.io@proton.me>
2026-06-19 19:07:07 +0330

    Handling bounderies and Free buffer via pressing Insert

--- src/buffer.rs
+++ src/buffer.rs
@@ -8,29 +8,39 @@ pub use crossterm::{
         execute,
         cursor::MoveTo
 
-    };      
+};
+use std::io::Write;
+const MARGIN: (u16, u16, &str) = (5, 2, "&"); 
+const WARNING: (u16, u16) = (10, 10);
 
-/*fn take(strr: String) -> String 
+pub fn buffer(stream: &mut std::io::Stdout, size: (u16, u16)) -> String
 {
-    strr
-}*/
-
-pub fn buffer(stream: &mut std::io::Stdout) -> String
-{
-    use std::io::Write;
     terminal::enable_raw_mode();
 
     let mut buffer = String::new(); 
-
     execute!(stream, EnableMouseCapture); 
-
-    let (mut current_width, mut current_height) = (1, 10);
+    let (mut current_width, mut current_height) = (3, size.1 - MARGIN.0 - 1);
+    let mut free = false;
+    /*warp it in a if statement so you can't just wonder around unless I want or You want*/
 
     loop
     {
             if current_width   == 0 { current_width  = 1; }
         else if current_height == 0 { current_height = 1; }
-        /* preventing a runtime panick, see moving keys */ 
+        /* preventing a runtime panick, see moving keys */
+
+        if buffer.len() as u16 > size.0 * 3 - 19 
+        {
+            buffer.pop(); 
+        }
+
+        if current_height > size.1 - MARGIN.0 && current_height < size.1 - MARGIN.1
+        {
+            stream.queue(MoveTo(1, current_height));
+            stream.write(MARGIN.2.as_bytes());
+            stream.queue(MoveTo(size.0 - 1, current_height));
+            stream.write(MARGIN.2.as_bytes());
+        }
 
         match read().unwrap()
         {
@@ -47,7 +57,11 @@ pub fn buffer(stream: &mut std::io::Stdout) -> String
                         else
                         {
                             buffer.push(key);
-                            execute!(stream, Clear(ClearType::CurrentLine), MoveTo(current_width, current_height));
+                            if current_width > size.0 - 4
+                            {
+                                current_width = 3;
+                            }
+                            execute!(stream, Clear(ClearType::UntilNewLine), MoveTo(current_width, current_height));
                             stream.write(buffer.as_bytes());
                         }
                     },
@@ -60,29 +74,39 @@ pub fn buffer(stream: &mut std::io::Stdout) -> String
                     KeyCode::Backspace => 
                     {
                         buffer.pop().unwrap();
-                        execute!(stream, Clear(ClearType::CurrentLine), MoveTo(current_width, current_height));
+                        execute!(stream, Clear(ClearType::UntilNewLine), MoveTo(current_width, current_height));
                         stream.write(buffer.as_bytes());
                     }, 
                     KeyCode::Enter =>
                     {
-                        execute!(stream, Clear(ClearType::All));
-                        buffer = String::new();
+                        execute!(stream, DisableMouseCapture);
+                        terminal::disable_raw_mode();
+
+                        stream.queue(MoveTo(0, size.1));
+
                         return buffer;
                     },
-                    KeyCode::Insert =>
+                    KeyCode::Delete =>
                     {
                         execute!(stream, Clear(ClearType::All), MoveTo(current_width, current_height));
                         stream.write(buffer.as_bytes());
-                    }
+                    }, 
+                    KeyCode::Insert =>
+                    {
+                        free = true;
+                    },
                     navkey @ (KeyCode::Right | KeyCode::Left | KeyCode::Up | KeyCode::Down)  => 
                     {
-                        match navkey
+                        if free
                         {
-                            KeyCode::Right => current_width  += 1, 
-                            KeyCode::Left  => current_width  -= 1, 
-                            KeyCode::Down  => current_height += 1,
-                            KeyCode::Up    => current_height -= 1, 
-                            _ => {},
+                            match navkey
+                            {
+                                KeyCode::Right => current_width  += 1, 
+                                KeyCode::Left  => current_width  -= 1, 
+                                KeyCode::Down  => current_height += 1,
+                                KeyCode::Up    => current_height -= 1, 
+                                _ => {},
+                            }
                         }
                         stream.queue(MoveTo(current_width, current_height));
                         /*You can navigate in terminal, but you cannot delete characters! for now*/
@@ -97,8 +121,10 @@ pub fn buffer(stream: &mut std::io::Stdout) -> String
                 {
                     MouseEventKind::Down(MouseButton::Left) => 
                     {
-                        current_width = event.column;
-                        current_height = event.row;
+                        if free { 
+                            current_width = event.column;
+                            current_height = event.row;
+                        }
                         stream.queue(MoveTo(current_width, current_height));
                     }, 
                     _ => {}, //buffer = format!("{:?}", event), 
